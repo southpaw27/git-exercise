33218ee37df4cd94c7391deab6f2933adc6fda6b
southpaw27 <awkelley27@gmail.com>
2013-09-05 22:56:17 -0500

    Added class HelloWorld

---
+++ src/edu/grinnell/csc207/kelleyan/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package edu.grinnell.csc207.kelleyan.hello;
+
+public class HelloWorld {
+	
+	 public static void main(String[] args) {
+	        System.out.println("Hello, GitHub!");
+	    } // main(String[])
+
+}
