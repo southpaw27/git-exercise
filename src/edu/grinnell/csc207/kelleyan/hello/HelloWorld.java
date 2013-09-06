2c17c040e1a1979f367903264149522b122cad7f
Andrew Kelley <awkelley27@gmail.com>
2013-09-05 22:58:10 -0500

    Update HelloWorld.java

--- src/edu/grinnell/csc207/kelleyan/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/kelleyan/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.grinnell.csc207.kelleyan.hello;
 public class HelloWorld {
 	
 	 public static void main(String[] args) {
-	        System.out.println("Hello, GitHub!");
+	        System.out.println("Hello, GitHub! I made a small change!");
 	    } // main(String[])
 
 }
