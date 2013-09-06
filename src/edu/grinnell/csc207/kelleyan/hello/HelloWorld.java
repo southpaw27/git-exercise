7a6de7366fccc43b8793f6f3902b79cf7a1b5397
Andrew Kelley <awkelley27@gmail.com>
2013-09-05 23:02:51 -0500

    Update HelloWorld.java

--- src/edu/grinnell/csc207/kelleyan/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/kelleyan/hello/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	
 	 public static void main(String[] args) {
 	        System.out.println("Hello, GitHub! I made a small change!");
+	        System.out.println("This is an extra print statement");
 	    } // main(String[])
 
 }
